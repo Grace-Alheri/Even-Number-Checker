c7a738fc71dac21ad7c10ccbe0564c86ea374871
Grace-Alheri <graceotitoju843@gmail.com>
2025-02-13 16:21:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$oddnumber = 7;
+if ($oddnumber % 2 != 0) {
+    echo "$oddnumber is an odd number";
+} else {
+    echo "$oddnumber is an even number";
+}
+
+echo "<br>";
+
+$evennumber = 10;
+if ($evennumber % 2 == 0) {
+    echo "$evennumber is an even number";
+} else {
+    echo "$evennumber is an odd number";
+}
+
+?>
